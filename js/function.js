d95543b7e466b49126b52fc1827b8770b676ebbd
DunderBoy <heden.johan@gmail.com>
2016-01-27 22:05:42 +0100

    added services and location section layouts

--- js/function.js
+++ js/function.js
@@ -1,22 +1,28 @@
 function changeMainLink(activeLink) {
-	switch(activeLink) {
+	switch (activeLink) {
 	case link1:
 		document.getElementById('link1').className = "active";
 		document.getElementById('link2').className = "";
 		document.getElementById('link3').className = "";
-		console.log("link1");
+		document.getElementById('link4').className = "";
 		break;
 	case link2:
 		document.getElementById('link2').className = "active";
 		document.getElementById('link1').className = "";
 		document.getElementById('link3').className = "";
-		console.log("Link2");
+		document.getElementById('link4').className = "";
 		break;
 	case link3:
 		document.getElementById('link3').className = "active";
 		document.getElementById('link2').className = "";
 		document.getElementById('link1').className = "";
-		console.log("Link3");
-		break;	
+		document.getElementById('link4').className = "";
+		break;
+    case link4:
+        document.getElementById('link4').className = "active";
+		document.getElementById('link2').className = "";
+		document.getElementById('link1').className = "";
+		document.getElementById('link3').className = "";
+		break;
 	}
 }
